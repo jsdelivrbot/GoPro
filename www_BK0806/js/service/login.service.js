24372180b517099e9c0d3a7627bafcd81fa2de55
Admin <rameshkror@gmail.com>
2018-07-09 11:08:05 +0530

    Initial Commit

---
+++ www_BK0806/js/service/login.service.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+rico.factory('loginservice', function ($rootScope, $http) {
+
+    var service = {
+
+        login: function (data, success, failure) {
+           
+            $http.post(baserURL() + "Login/MobileLogin", data).success(success).error(failure);
+            
+        }
+    }
+
+    return service;
+});
